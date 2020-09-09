9b1c46308824d2607d169ab1b625b2dbc36c571d
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-10 02:11:06 +0530

    [Saurabh] Each month would like to know the days won and lost and by how much

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -5,13 +5,15 @@ class GamblingSimulation
 //US1 Initialising stack and betting amount
 	static final int STACK_AMOUNT = 100 ;
 	static final int BETTING_AMOUNT = 1 ;
-	static final int DAYS_IN_MONTH = 20 ;
+	static final int DAYS_IN_MONTH = 30 ;
 
 	public static void GamblingGame()
 	{
 		int game_result;
+		int monthly_investment_amount = 0;
 		int total_amount = 0;
-		for(int day = 1 ; day < DAYS_IN_MONTH ; day++)
+// UC4 for a month find total amount 
+		for(int day = 1 ; day <= DAYS_IN_MONTH ; day++)
 			{
 				game_result = STACK_AMOUNT ;
 //UC3 resign for day if stack reaches more of less than 50%
@@ -36,9 +38,18 @@ class GamblingSimulation
 				{
 					System.out.println("Result of day :"+day+" is Gambler loss $"+game_result);
 				}
+//UC5 Total month result by how much amount
+				monthly_investment_amount = monthly_investment_amount + STACK_AMOUNT;
 				total_amount = total_amount + game_result ;
 			}
-			System.out.println("Total Amount after 20 days " +total_amount);
+			System.out.println("Total Amount after a month " +total_amount);
+			System.out.println("Total investment Amount after a month " +monthly_investment_amount);
+			if(total_amount > monthly_investment_amount )
+			{
+				System.out.println("Gambler won $"+(total_amount-monthly_investment_amount));
+			}
+			else
+				System.out.println("Gambler lost $"+(monthly_investment_amount-total_amount));
 	}
 
 	public static void main(String args[])
