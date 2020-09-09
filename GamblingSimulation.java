37734a63bf945b5210e5e4770c798a0c6a154ca4
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-10 00:17:37 +0530

    [Saurabh] As a Calculative Gambler if won or lost 50% of the stake, would resign for the day

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -10,17 +10,21 @@ class GamblingSimulation
 	{
 		int game_result;
 		game_result = STACK_AMOUNT ;
-		double game_check = Math.floor(Math.random()*10)%2;
+//UC3 resign for day if stack reaches more of less than 50%
+		while( game_result > (STACK_AMOUNT/2) && game_result < (STACK_AMOUNT+(STACK_AMOUNT/2)) )
+		{
+			double game_check = Math.floor(Math.random()*10)%2;
 //UC2 Betting $1 so either wins or lose
-		if( game_check == 1 )
-		{
-			game_result = game_result + BETTING_AMOUNT ;
+			if( game_check == 1 )
+			{
+				game_result = game_result + BETTING_AMOUNT ;
+			}
+			else
+			{
+				game_result = game_result - BETTING_AMOUNT ;
+			}
 		}
-		else
-		{
-			game_result = game_result - BETTING_AMOUNT ;
-		}
-		System.out.println("Result of the game :"+game_result);
+		System.out.println("Result of the day :"+game_result);
 	}
 
 	public static void main(String args[])
