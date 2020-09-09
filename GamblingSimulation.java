2fad03f3fed8f10ebe497edb46a247745642375a
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-09 23:24:32 +0530

    [Saurabh] As a Gambler, would start with a stake of 00 every day and bet  every game

---
+++ GamblingSimulation.java
@@ -0,0 +1,13 @@
+import java,util.*;
+
+class GamblingSimulation
+{
+//US1 Initialising stack and betting amount
+	final int STACK_AMOUNT = 100 ;
+	final int BETTING_AMOUNT = 1 ;
+
+}
+public static void main(String args[])
+{
+	GamblingSimulation GamSim = new GamblingSimulation();
+}
