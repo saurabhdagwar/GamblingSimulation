744e79cdfd94a96510faa6fe4a2a094227d2666f
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-16 13:39:19 +0530

    [Saurabh]Refactored: Proper spacing and Variables in Camel Cases

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -3,45 +3,43 @@ import java.util.*;
 class GamblingSimulation
 {
 //US1 Initialising stack and betting amount
-	static final int STACK_AMOUNT = 100 ;
-	static final int BETTING_AMOUNT = 1 ;
-	static final int DAYS_IN_MONTH = 20 ;
-
+	static final int STACKAMOUNT = 100 ;
+	static final int BETTINGAMOUNT = 1 ;
+	static final int DAYSINMONTH = 20 ;
 	public static void GamblingGame()
 	{
-		int game_result;
-		int total_amount = 0;
+		int gameresult;
+		int totalamount = 0;
 //UC4 perform whole operation for a month
-		for(int day = 1 ; day < DAYS_IN_MONTH ; day++)
+		for(int day = 1 ; day < DAYSINMONTH ; day++)
 			{
-				game_result = STACK_AMOUNT ;
+				gameresult = STACKAMOUNT ;
 //UC3 resign for day if stack reaches more of less than 50%
-				while( game_result > (STACK_AMOUNT/2) && game_result < (STACK_AMOUNT+(STACK_AMOUNT/2)) )
+				while( gameresult > (STACKAMOUNT/2) && gameresult < (STACKAMOUNT+(STACKAMOUNT/2)) )
 				{
-					double game_check = Math.floor(Math.random()*10)%2;
+					double gamecheck = Math.floor(Math.random()*10)%2;
 //UC2 Betting $1 so either wins or lose
-					if( game_check == 1 )
+					if( gamecheck == 1 )
 					{
-						game_result = game_result + BETTING_AMOUNT ;
+						gameresult = gameresult + BETTINGAMOUNT ;
 					}
 					else
 					{
-						game_result = game_result - BETTING_AMOUNT ;
+						gameresult = gameresult - BETTINGAMOUNT ;
 					}
 				}
-				if(game_result > STACK_AMOUNT )
+				if(gameresult > STACKAMOUNT )
 				{
-					System.out.println("Result of day :"+day+" is  Gambler Won $"+game_result);
+					System.out.println("Result of day :"+day+" is  Gambler Won $"+gameresult);
 				}
 				else
 				{
-					System.out.println("Result of day :"+day+" is Gambler loss $"+game_result);
+					System.out.println("Result of day :"+day+" is Gambler loss $"+gameresult);
 				}
-				total_amount = total_amount + game_result ;
+				totalamount = totalamount + gameresult ;
 			}
-			System.out.println("Total Amount after 20 days " +total_amount);
+			System.out.println("Total Amount after 20 days " +totalamount);
 	}
-
 	public static void main(String args[])
 	{
 		GamblingSimulation GamSim = new GamblingSimulation();
