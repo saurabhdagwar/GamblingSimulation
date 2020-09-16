da8346461b2446bb9565a4a716cb67e7eddba339
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-16 13:30:43 +0530

    [Saurabh]Fixed: Proper Spacing

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,11 +1,9 @@
 import java,util.*;
-
 class GamblingSimulation
 {
 //US1 Initialising stack and betting amount
 	static final int STACK_AMOUNT = 100 ;
 	static final int BETTING_AMOUNT = 1 ;
-
 }
 public static void main(String args[])
 {
